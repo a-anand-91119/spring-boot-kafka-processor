8250f382db83057cc3b203f22ca296e49f0d28d3
A Anand <a.anand.91119@gmail.com>
2025-03-16 08:59:16 +0530

    Adding proto

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import com.google.protobuf.gradle.id
+
 plugins {
 	java
 	alias(libs.plugins.springframework.boot)
 	alias(libs.plugins.spring.dependency.management)
 	alias(libs.plugins.spotless)
+	alias(libs.plugins.google.protobuf)
 }
 
 group = "dev.notyouraverage.project.one.kafka.processor"
@@ -24,11 +27,15 @@ repositories {
 	mavenCentral()
 }
 
+extra["springGrpcVersion"] = "0.5.0"
+
 dependencies {
 	implementation(libs.spring.boot.actuator)
+	implementation(libs.spring.boot.grpc)
 	implementation(libs.spring.boot.web)
 	implementation(libs.spring.kafka)
 	implementation(libs.springdoc.webmvc.ui)
+	implementation(libs.grpc.services)
 
 	implementation(libs.opentelemetry.exporter.otlp)
 	implementation(libs.micrometer.tracing.bridge.otel)
@@ -40,7 +47,35 @@ dependencies {
 
 	testImplementation(libs.spring.boot.test)
 	testImplementation(libs.spring.kafka.test)
+	testImplementation(libs.spring.grpc.test)
 	testRuntimeOnly(libs.junit.platform.launcher)
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.grpc:spring-grpc-dependencies:${property("springGrpcVersion")}")
+	}
+}
+
+protobuf {
+	protoc {
+		artifact = "com.google.protobuf:protoc"
+	}
+	plugins {
+		id("grpc") {
+			artifact = "io.grpc:protoc-gen-grpc-java"
+		}
+	}
+	generateProtoTasks {
+		all().forEach {
+			it.plugins {
+				id("grpc") {
+					option("jakarta_omit")
+					option("@generated=omit")
+				}
+			}
+		}
+	}
 }
 
 tasks.withType<Test> {
@@ -54,4 +89,8 @@ spotless {
 		trimTrailingWhitespace()
 		endWithNewline()
 	}
+}
+
+tasks.named("generateProto") {
+	finalizedBy("spotlessApply")
 }
